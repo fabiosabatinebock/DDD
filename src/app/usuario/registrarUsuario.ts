6e38e87226cddeb07b11da8ae7c0e1c5aa53d35a
Fabio Bock <fabio-sabatine@hotmail.com>
2024-03-05 22:39:32 -0300

    Continuação do Curso

--- src/app/usuario/registrarUsuario.ts
+++ src/app/usuario/registrarUsuario.ts
@@ -5,10 +5,10 @@ import RegistrarUsuario from "@/core/usuario/service/RegistrarUsuario";
 export default async function registrarUsuario(){
     terminalUtil.titulo("Registrar Usuário")
 
-    const id = await terminalUtil.campoRequerido("Id: ")
-    const nome = await terminalUtil.campoRequerido("Nome: ")
-    const email = await terminalUtil.campoRequerido("Email: ")
-    const senha = await terminalUtil.campoRequerido("Senha: ")
+    const id = await terminalUtil.campoRequerido("Id: ", "faseiffoea-sfasdlfs")
+    const nome = await terminalUtil.campoRequerido("Nome: ", "Ana da Silva")
+    const email = await terminalUtil.campoRequerido("Email: ", "ana.silva@empresa.com.br")
+    const senha = await terminalUtil.campoRequerido("Senha: ", "123456")
 
     const usuario: Usuario = {id, nome, email, senha}
 
--- src/core/usuario/service/RegistrarUsuario.ts
+++ src/core/usuario/service/RegistrarUsuario.ts
@@ -1,9 +1,13 @@
-import CasoDeUso from "@/core/shared/CasoDeUso";
-import Usuario from "../model/Usuario";
+import CasoDeUso from "@/core/shared/CasoDeUso"
+import Usuario from "../model/Usuario"
+import RepositorioUsuarioEmMemoria from "./RepositorioUsuarioEmMemoria"
 
 export default class RegistrarUsuario implements CasoDeUso<Usuario, void>{
     async executar(Usuario: Usuario): Promise<void> {
         const senhaCripto = Usuario.senha.split('').reverse().join('')
+        const repo = new RepositorioUsuarioEmMemoria()
+        const usuarioExistente = repo.buscarPorEmail(Usuario.email)
+
         console.log(`\n\n${senhaCripto}`)
     }
 }
